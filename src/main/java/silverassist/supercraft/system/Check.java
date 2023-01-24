85d0f64d9d4d69c62528361500cd8c19b00c6718
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-24 20:11:59 +0900

    縦横幅が異なるときに正常に検知されないバグ, 正常に比較されないミスを修正

--- src/main/java/silverassist/supercraft/system/Check.java
+++ src/main/java/silverassist/supercraft/system/Check.java
@@ -1,28 +1,41 @@
 package silverassist.supercraft.system;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import silverassist.supercraft.Util;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Check {
+    private static final ItemStack AIR = new ItemStack(Material.AIR,0);
     private static int[] slotPlace = Util.getRectSlotPlaces(0,5,5);
+
     public static String table(Inventory inv){
-        ItemStack[] invItems = new ItemStack[25];
         int[] side = rectSlot(inv);
-        Map<String,ItemStack[][]> candidate = Recipe.getRawItems();
-        for(int i =side[0];i<side[1];i++){
-            for(int j = side[2];j<side[3];j++){
-                if(candidate.size()==0)return null;
-                ItemStack item = inv.getItem(slotPlace[i*5+j]);
-                int h = i - side[0], w= j -side[2];
-                candidate.forEach((key, value) ->{
-                    if(!item.equals(value[h][w]))candidate.remove(key);
-                });
+        Map<String,ItemStack[][]> candidate = new HashMap<>(Recipe.getRawItems());
+        if(side == null)return null;
+        new HashMap<>(candidate).forEach((key, value) ->{
+            if(value.length!= (side[1]-side[0]) || value[0].length!= (side[3]-side[2])){
+                candidate.remove(key);
+                return;
             }
-        }
+            System.out.println(Arrays.toString(side));
+            for(int i =side[0];i<side[1];i++){
+                int h = i - side[0];
+                for(int j = side[2];j<side[3];j++){
+                    ItemStack item = inv.getItem(slotPlace[i*5+j]) == null ? AIR : inv.getItem(slotPlace[i*5+j]);
+                    int w= j -side[2];
+                    if(value.length <= h || value[h].length <= w || !item.equals(value[h][w])){
+                        candidate.remove(key);
+                        return;
+                    };
+                }
+            }
+        });
         Set<String> keys = candidate.keySet();
         if(keys.size() ==1){
             return (String)keys.toArray()[0];
@@ -61,11 +74,11 @@ public class Check {
         }
 
         if(side[2]==5)return null;
-        for(int i = 5*side[1] -1;true;){
+        for(int i = 5*side[1]- 1;true;){
             if(inv.getItem(slotPlace[i]) == null){
                 if((i-=5)<5*side[0]){
                     if(i%5==side[2])break;
-                    i = 5 * side[1] +(i%5-1);
+                    i = 5 * (side[1]-1) +((i+5)%5-1);
                 }
             }else{
                 side[3] = i %5 +1;
