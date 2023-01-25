32ccdc9fe8a6efa1b9b4b4028dc1e9bec1d59587
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-25 15:52:26 +0900

    製作可能･不可機能に基づく小変更

--- src/main/java/silverassist/supercraft/menu/admin/SetCraft.java
+++ src/main/java/silverassist/supercraft/menu/admin/SetCraft.java
@@ -1,6 +1,7 @@
 package silverassist.supercraft.menu.admin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,26 +18,30 @@ import silverassist.supercraft.Util;
 import silverassist.supercraft.system.Check;
 import silverassist.supercraft.system.Recipe;
 
-import java.util.Arrays;
 
 public class SetCraft {
     private final JavaPlugin plugin;
     private final Player P;
     private final String ID;
+    private final YamlConfiguration YML;
+    private final ItemStack GUI_ENABLE = Util.createItem(Material.LIME_STAINED_GLASS_PANE,"§a§l製作可能");
+    private final ItemStack GUI_DISABLE = Util.createItem(Material.RED_STAINED_GLASS_PANE,"§c§l製作不可");
+
     public SetCraft(JavaPlugin plugin, Player p, String id){
         this.plugin = plugin;
         this.P = p;
         this.ID =id;
+        this.YML = CustomConfig.getYmlByID(id);
         p.closeInventory();
         plugin.getServer().getPluginManager().registerEvents(new listener(),plugin);
     }
 
     public void open(){
         Inventory inv = Bukkit.createInventory(P,45, Util.PREFIX+"§r§d§l"+ID+"§a§lのクラフト編集");
-        for(int i = 0;i<20;i++)inv.setItem(5 + 9*(i/4) + i%4, Util.GUI_BG);
-        inv.setItem(24,Recipe.getCraftItem(ID));
-        ItemStack[][] items = Recipe.getRawItem(ID);
-        for(int i =0;i<items.length;i++)for(int j = 0;j<items[i].length;j++)inv.setItem(9 * i + j,items[i][j]);
+        for(int slot:Util.getRectSlotPlaces(5,4,5))inv.setItem(slot, Util.GUI_BG);
+        inv.setItem(24,YML.getItemStack("item.single"));
+        for(int i =0;i<25;i++)inv.setItem(9 * (i/5) + i%5, YML.getItemStack("raw."+i/5+i%5));
+        inv.setItem(44,YML.getBoolean("isEnable",true) ? GUI_ENABLE : GUI_DISABLE);
         P.openInventory(inv);
     }
 
@@ -48,7 +53,6 @@ public class SetCraft {
             Inventory inv = e.getInventory();
             int[] side = Check.rectSlot(inv);
             if(side==null)return;
-            e.getPlayer().sendMessage(Arrays.toString(side));
             YamlConfiguration yml = CustomConfig.getYmlByID(ID);
             for(int i=0;i<25;i++)yml.set("raw."+i/5+i%5,null);
             for(int x = side[0];x<side[1];x++){
@@ -57,7 +61,7 @@ public class SetCraft {
                     yml.set("raw."+(x - side[0])+(y - side[2]),item);
                 }
             }
-            yml.set("item",inv.getItem(24));
+            yml.set("item.single",inv.getItem(24));
             yml.set("raw.h",side[1]-side[0]);
             yml.set("raw.w",side[3]-side[2]);
             CustomConfig.saveYmlByID(ID);
@@ -70,6 +74,12 @@ public class SetCraft {
             if(e.getCurrentItem() == null || !e.getClickedInventory().getType().equals(InventoryType.CHEST))return;
             int slot = e.getSlot();
             if(slot%9 > 4 && slot!=24)e.setCancelled(true);
+            if(slot == 44){
+                boolean toEnable = e.getCurrentItem().getType() == Material.RED_STAINED_GLASS_PANE;
+                e.getClickedInventory().setItem(44, toEnable ? GUI_ENABLE : GUI_DISABLE);
+                CustomConfig.getYmlByID(ID).set("isEnable",toEnable);
+                CustomConfig.saveYmlByID(ID);
+            }
         }
     }
 }
